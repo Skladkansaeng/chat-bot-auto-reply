196eadbce8e87e54d6b37ab58430b6e4b31615de
settakarn kladkansaeng <59011344@kmitl.ac.th>
2021-03-05 23:22:01 +0700

    update:path module

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/userinfo', require('./userinfo'))
+router.use('/userinfo', require('./UserInfo'))
 router.use('/webhook', require('./webhook'))
 
 module.exports = router
