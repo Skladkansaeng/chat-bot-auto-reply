2a2380c2c2b3ddd79068fecd38055d31228dbbc8
Settakarn Kladkansaeng <settakarn@angstromsolutions.net>
2021-03-09 16:59:41 +0700

    update: fix error

--- helpers/jibApi.js
+++ helpers/jibApi.js
@@ -34,7 +34,7 @@ const pushMessageClaim = (userId) => {
             claims = JSON.parse(body)
         }
         catch {
-            claims = [{ claimno: 'CL-20210202714', claimsname: 'GG XTRFY GAMING MOUSE M4 RGB RETRO 1-Y' }]
+            claims = [{ claimno: 'CL-20210202714', claimname: 'GG XTRFY GAMING MOUSE M4 RGB RETRO 1-Y' }]
         }
 
         if (claims.length !== undefined)
