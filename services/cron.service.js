62edca4ffccc367700dc3d54f511319ad074f8d4
Settakarn Kladkansaeng <settakarn@angstromsolutions.net>
2021-03-09 17:14:43 +0700

    update: cron

--- services/cron.service.js
+++ services/cron.service.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { getUsers } = require('../helpers/users')
 
 
-const task = cron.schedule('0 0 */5 * * *', () => {
+const task = cron.schedule('0 */10 * * * *', () => {
   getUsers().forEach(({ userId }) => {
     require('../helpers/jibApi').pushMessageClaim(userId)
   })
