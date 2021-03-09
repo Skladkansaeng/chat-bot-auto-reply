5fc61f0f1f374734fdd3e00412316a5de8eba7cc
Settakarn Kladkansaeng <settakarn@angstromsolutions.net>
2021-03-09 15:11:37 +0700

    update: fix error

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/userinfo', require('./UserInfo'))
+router.use('/userinfo', require('./userInfo'))
 router.use('/webhook', require('./webhook'))
 
 module.exports = router
